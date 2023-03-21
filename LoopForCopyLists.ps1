0ea341d43c0e9eba55fd1a437f60be64b696d9a3
esraa224455 <esraa.elhabash21@gmail.com>
2023-03-21 12:02:42 +0200

    NewCommmmit

---
+++ LoopForCopyLists.ps1
@@ -0,0 +1,39 @@
+﻿
+#Parameters
+$SourceSiteURL = "https://t6syv.sharepoint.com/sites/EsraaTeamSite"
+$DestinationSiteURL = "https://t6syv.sharepoint.com/sites/TargetSite"
+  
+#Connect to the source Site
+Connect-PnPOnline -URL $SourceSiteURL -UseWebLogin
+  
+#Get all document libraries
+$SourceLists =  Get-PnPList -Includes RootFolder | Where {$_.BaseType -eq "GenericList" -and $_.Hidden -eq $False} | Select Title, Description, ItemCount
+#Connect to the destination site
+Connect-PnPOnline -URL $DestinationSiteURL -UseWebLogin
+  
+#Get All Lists in the Destination site
+$DestinationLists = Get-PnPList
+  
+ForEach($SourceList in $SourceLists)
+{
+    #Check if the library already exists in target
+    If(!($DestinationLists.Title -contains $DestinationLists.Title))
+    {
+        #Create a document library
+        $NewList  = New-PnPList -Title $SourceList.Title -Template GenericList
+        Write-host "Document List '$($SourceList.Title)' created successfully!" -f Green
+    }
+    else
+    {
+        Write-host "Document List '$($SourceList.Title)' already exists!" -f Yellow
+    }
+  
+    #Get the Destination Library
+    $DestinationList = Get-PnPList $SourceList.Title -Includes RootFolder
+    $SourceListURL = $SourceList.RootFolder.ServerRelativeUrl
+    $DestinationListURL = $DestinationList.RootFolder.ServerRelativeUrl
+  
+    #Copy All Content from Source Library to Destination library
+    Copy-PnPFile -SourceUrl $SourceListURL -TargetUrl $DestinationListURL -SkipSourceFolderName -Force -OverwriteIfAlreadyExists
+    Write-host "`tContent Copied from $SourceListURL to  $DestinationListURL Successfully!" -f Green
+}
